c21e4a30adaecbf24dc03d441262e58636af240b
navnoorsingh0309 <118381310+navnoorsingh0309@users.noreply.github.com>
2023-07-10 18:37:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+var menu = 0;
+
+function menuClick() {    
+    var bar_one = document.getElementById("menu_1");
+    bar_one.style.transition = "transform 0.1s";    
+    var bar_two = document.getElementById("menu_2");
+    bar_two.style.transition = "transform 0.1s";    
+    var bar_three = document.getElementById("menu_3");
+    bar_three.style.transition = "transform 0.1s";
+    var menuBlock = document.getElementById("Menu_block");
+    if(menu==0)
+    {
+        bar_one.style.transform = "rotateZ(45deg) translate(8px, 0px)";
+        bar_two.style.transform = "rotateZ(-45deg)";
+        bar_three.style.transform = "rotateZ(45deg) translate(0px, -15.5px)";
+        menuBlock.style.animation = "drop_down_animation 1s ease-in-out";
+        menuBlock.style.display ="block";
+        menu = 1;
+    }
+    else
+    {
+        bar_one.style.transform = "rotateZ(0deg) translate(0px, 0px)";
+        bar_two.style.transform = "rotateZ(0deg)";
+        bar_three.style.transform = "rotateZ(0deg) translate(0px, 0px)";
+        menuBlock.style.display = "none";
+        menu = 0;
+    }
+}
