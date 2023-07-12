a51d6f732530906f81c4c98cb60fef3da1fdf9ac
navnoorsingh0309 <118381310+navnoorsingh0309@users.noreply.github.com>
2023-07-12 18:37:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-var menu = 0;
-
-function menuClick() {    
-    var bar_one = document.getElementById("menu_1");
-    bar_one.style.transition = "transform 0.1s";    
-    var bar_two = document.getElementById("menu_2");
-    bar_two.style.transition = "transform 0.1s";    
-    var bar_three = document.getElementById("menu_3");
-    bar_three.style.transition = "transform 0.1s";
-    var menuBlock = document.getElementById("Menu_block");
-    if(menu==0)
-    {
-        bar_one.style.transform = "rotateZ(45deg) translate(8px, 0px)";
-        bar_two.style.transform = "rotateZ(-45deg)";
-        bar_three.style.transform = "rotateZ(45deg) translate(0px, -15.5px)";
-        menuBlock.style.animation = "drop_down_animation 1s ease-in-out";
-        menuBlock.style.display ="block";
-        menu = 1;
+//scroll function
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var messages = $(".Messages_Section");
+    for(var i=0; i<messages.length;i++) {
+        var message = messages[i];
+        if($(message).position().top <pageBottom)
+        {
+            $(message).addClass("visible");
+        }
+        else
+        {
+            $(message).removeClass("visible");
+        }
     }
-    else
-    {
-        bar_one.style.transform = "rotateZ(0deg) translate(0px, 0px)";
-        bar_two.style.transform = "rotateZ(0deg)";
-        bar_three.style.transform = "rotateZ(0deg) translate(0px, 0px)";
-        menuBlock.style.display = "none";
-        menu = 0;
-    }
-}
+});
