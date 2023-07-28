6e77a1624bb0bb21be9ca36ede5bb028f464ab4e
navnoorsingh0309 <118381310+navnoorsingh0309@users.noreply.github.com>
2023-07-28 12:20:11 +0530

    Create Team.js

---
+++ Team/Team.js
@@ -0,0 +1,16 @@
+var card = document.getElementsByClassName("card-Team");
+var bounds;
+function BubblyFunc(e, n) {
+    bounds = card[n].getBoundingClientRect();
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
+    var leftX = mouseX - bounds.left;
+    var leftY = mouseY - bounds.top;
+    var centerX = leftX - bounds.width/2;
+    var centerY = leftY - bounds.height/2;
+    var distance = Math.sqrt(centerX**2 + centerY**2);
+    card[n].style.transform = "scale3d(1.0, 1.0, 1.0) rotate3d("+centerX/10+","+centerY/10+", 0, "+(Math.log(distance)*2)+"deg)";
+}
+function BubblyOut(n) {
+    card[n].style.transform = "";
+}
